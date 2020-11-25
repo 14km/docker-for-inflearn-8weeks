43dcaa6f0cd71798c2e12c86828591ded46c059f
K-connor <k.connor614@gmail.com>
2020-11-26 00:26:49 +0900

    front를 위한 코드 추가.

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,68 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('/api/hi').then(response => {
+      console.log('response', response);
+    });
+  }, []);
+
+  // api call을 미리 가져올 수 있게한다.
+  useEffect(() => {
+    axios.get('/api/values').then(response => {
+      console.log('response', response);
+      setLists(response.data);
+    });
+  }, []);
+
+  const [lists, setLists] = useState([]);
+  const [value, setValue] = useState('');
+
+  const changeHandler = (event) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    axios.post('/api/value', {value: value}).then(response => {
+      if (response.data.success) {
+        console.log('response', response);
+        setLists([...lists, response.data]);
+        setValue('');
+      }
+      else {
+        alert('값을 DB에 넣는데 실패했습니다.');
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <div className="container">
+            {lists && lists.map((list, index) => (
+                <li key={index}>{list.value} </li>
+            ))}
+            <br/>
+            안녕하세요.
+            <form className="example" onSubmit={submitHandler}>
+              <input
+                  type="text"
+                  placeholder="입력해주세요..."
+                  onChange={changeHandler}
+                  value={value}
+              />
+              <button type="submit">확인.</button>
+            </form>
+          </div>
+        </header>
+      </div>
   );
 }
 
